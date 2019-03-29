55fc31e92e41c214243854f319bdeae6726c8408
zye <2223734457@qq.com>
2019-03-29 13:14:42 +0800

    赵勇儿测试前提交

--- pyg_shop_web/src/main/webapp/js/controller/statisticController.js
+++ pyg_shop_web/src/main/webapp/js/controller/statisticController.js
@@ -1,4 +1,5 @@
 app.controller('statisticController',function ($scope,statisticService) {
+    //测试拉取
     //读取列表数据绑定到表单中
     $scope.findAll=function(){
         statisticService.findAll().success(
