56ba8094c463f454cd181299cc487abfb38f3124
zye <2223734457@qq.com>
2019-03-29 20:35:55 +0800

    赵勇儿itemsku,数据库查询完成提交

---
+++ pyg_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/StatisticServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.TbItemMapper;
+import com.pinyougou.pojo.TbItem;
+import com.pinyougou.pojo.TbItemExample;
+import com.pinyougou.sellergoods.service.StatisticService;
+
+
+
+import java.util.List;
+
+@Service
+public class StatisticServiceImpl implements StatisticService {
+
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    @Override
+    public List findAll(String sellerId) {
+        TbItemExample example = new TbItemExample();
+        example.createCriteria().andSellerIdEqualTo(sellerId);
+        List<TbItem> tbItems = itemMapper.selectByExample(example);
+        return tbItems;
+    }
+}
